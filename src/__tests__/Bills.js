2778d197b8e2dfc61f2859c616d3fa745baddd5f
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-04-26 21:50:06 +0200

    - [ ]  composant views/Bills : faire passer le taux de couverture à 100%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,6 +3,10 @@ import BillsUI from "../views/BillsUI.js"
 import Bills from "../containers/Bills.js"
 import { bills } from "../fixtures/bills.js"
 import { localStorageMock } from "../__mocks__/localStorage"
+import userEvent from '@testing-library/user-event'
+import { ROUTES } from "../constants/routes"
+import firebase from "../__mocks__/firebase"
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -29,5 +33,61 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = html
       expect(screen.getAllByText('Erreur')).toBeTruthy()
     })
+    test(('should be sent to bill handleClickNewBill'), () => {
+        // Arrange
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Admin' }))
+      document.body.innerHTML = BillsUI({ data: bills })
+      const bill = new Bills({ document, onNavigate, localStorage });
+      const handleClickNewBill = jest.fn(bill.handleClickNewBill)
+      const disco = screen.getByTestId('btn-new-bill')
+      disco.addEventListener('click', handleClickNewBill)
+      userEvent.click(disco)
+      expect(handleClickNewBill).toHaveBeenCalled()
+    })
+    fit(('should be sent to bill handleClickIconEye'), () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Admin' }))
+      document.body.innerHTML = BillsUI({ data: bills })
+
+      const bill = new Bills({ document, onNavigate, localStorage });
+      const iconEye = screen.getAllByTestId('icon-eye')
+      
+      const handleClickIconEye = jest.fn(bill.handleClickNewBill)
+      
+      for (const icon of iconEye) {
+        icon.addEventListener('click', (e) => handleClickIconEye(icon))
+      }
+
+      userEvent.click(eye[0])
+      expect(handleClickIconEye).toHaveBeenCalled()
+/*       const modale = document.getElementsByClassName('modal-body')
+      expect(modale).toBeTruthy() */
+    })
+    fit(('should be sent sent to bill getBills'), () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Admin' }))
+      document.body.innerHTML = BillsUI({ data: bills })
+
+      const firestore = null;
+      const bill = new Bills({ document, onNavigate, firestore, localStorage });
+      const test = bill.getBills().then(data => expect(data).toEqual(bills));;
+      console.log(test);
+    })
+
+
   })
+
+
+
+
 })
